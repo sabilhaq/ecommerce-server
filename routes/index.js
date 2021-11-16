e91f7e868de23f0a24e8c26ab09d946cdf0f9c5b
Makrifat Sabil Haq <makrifatsabilhaq@gmail.com>
2021-11-16 21:15:33 +0700

    add feature login, create user

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const accessTokenSecret = 'secret';
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/auth', async function (req, res, next) {
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      res.status(500).json({ err: 'Invalid email or password' });
+    }
+
+    if (!user.verify(req.body.password)) {
+      res.status(500).json({ err: 'Invalid email or password' });
+    }
+
+    if (!user.token) {
+      const accessToken = jwt.sign({ email: user.email }, accessTokenSecret);
+      const filter = { email: req.body.email };
+      const update = { token: accessToken };
+
+      user = await User.findOneAndUpdate(filter, update, { new: true });
+    }
+
+    const userVerified = jwt.verify(user.token, accessTokenSecret);
+    if (!userVerified) {
+      const refreshToken = jwt.sign({ email: user.email }, accessTokenSecret);
+      const filter = { email: req.body.email };
+      const update = { token: refreshToken };
+
+      user = await User.findOneAndUpdate(filter, update, { new: true });
+    }
+
+    const response = {
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      token: user.token,
+    };
+
+    return res.json(response);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/', async function (req, res, next) {
+  try {
+    const user = await User.create(req.body);
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
