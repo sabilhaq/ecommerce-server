06b3a34814a1533dd7361bb1f02bc55e3b1d52a8
Makrifat Sabil Haq <makrifatsabilhaq@gmail.com>
2021-11-24 22:05:01 +0700

    add productsrouter to appjs

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const { graphqlHTTP } = require('express-graphql');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 
-var { schema, root } = require('./graphql/ProductSchema')
+var { schema, root } = require('./graphql/ProductSchema');
 
-main().catch(err => console.log(err));
+main().catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect('mongodb://localhost:27017/ecommercedb');
@@ -16,6 +18,7 @@ async function main() {
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var productsRouter = require('./routes/products');
 
 var app = express();
 
@@ -24,15 +27,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors())
+app.use(cors());
+app.use(fileUpload());
+app.use(bodyParser.json({ limit: '50mb' }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/products', productsRouter);
 
-app.use('/graphql', cors(), graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true
-}));
+app.use(
+  '/graphql',
+  cors(),
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
 
 module.exports = app;
